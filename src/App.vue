a03b3c28ebd6071ed5f142d484eb12f2d55673d4
thelostword <thelostword@163.com>
2022-06-13 11:46:06 +0800

    refactor: 暂存

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,11 @@
 <!--
  * @Author: losting
  * @Date: 2022-05-18 09:22:59
- * @LastEditTime: 2022-05-18 10:59:49
+ * @LastEditTime: 2022-06-13 11:45:39
  * @LastEditors: losting
  * @Description:
  * @FilePath: \vite-vue3-template\src\App.vue
 -->
-<script setup>
-</script>
-
 <template>
   <router-view />
 </template>
