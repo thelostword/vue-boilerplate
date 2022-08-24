1605a5477d9a19f259bdee60f6f94d1c015be34c
thelostword <thelostword@163.com>
2022-08-24 16:55:50 +0800

    refactor: 添加hooks外部依赖

--- src/hooks/use-request/index.js
+++ src/hooks/use-request/index.js
@@ -1,35 +1,11 @@
 /*
  * @Author: losting
  * @Date: 2022-06-22 15:00:07
- * @LastEditTime: 2022-06-22 15:21:52
- * @LastEditors: losting
+ * @LastEditTime: 2022-08-24 16:53:01
+ * @LastEditors: thelostword
  * @Description:
  * @FilePath: \vite-vue3-template\src\hooks\use-request\index.js
  */
-import { reactive, toRefs } from 'vue';
-import $to from '@/utils/await-to';
+import { useRequest as v3hooksUseRequest } from 'ahooks-vue';
 
-export default function useRequest(api) {
-  const state = reactive({
-    loading: false,
-    error: null,
-    data: null,
-  });
-
-  const run = async (params) => {
-    state.loading = true;
-    const [err, res] = await $to(api(params));
-    if (err) {
-      state.error = err;
-    } else {
-      state.data = res;
-      state.error = null;
-    }
-    state.loading = false;
-  };
-
-  return {
-    ...toRefs(state),
-    run,
-  };
-}
+export const useRequest = (...arg) => v3hooksUseRequest(...arg);
