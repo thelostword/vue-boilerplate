b59e778a5e6d3ada268bb68a86ab77d179154452
losting <moe.losting@gmail.com>
2023-06-21 16:32:17 +0800

    chore: update

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -4,13 +4,15 @@ const path = require('node:path');
 const createAliasSetting = require('@vue/eslint-config-airbnb/createAliasSetting');
 
 module.exports = {
+  root: true,
   env: {
     browser: true,
     es2022: true,
     node: true,
   },
   extends: [
-    'plugin:vue/vue3-essential',
+    'plugin:vue/vue3-recommended',
+    'eslint:recommended',
     '@vue/eslint-config-airbnb',
   ],
   settings: {
